41208dda6d7505a8bcc0f44237832185171e57dc
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2019-12-13 17:59:48 +0300

    Initial Commit

---
+++ RequestsLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RequestsLesson
+//
+//  Created by Ильдар Залялов on 13.12.2019.
+//  Copyright © 2019 Ildar Zalyalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
